6afc98afd59d2c4846e1dd7114ca407393ce4dbd
AZA Expédit <expeditaza@gmail.com>
2025-11-11 08:35:35 +0100

    Remove unused imports in models.py
    Commented out unused imports from django.db.models.

--- backend/attendance/models.py
+++ backend/attendance/models.py
@@ -5,15 +5,7 @@ from django.utils.translation import gettext_lazy as _
 from users.models import BaseModel, User
 
 from django.db import models
-from django.db.models import (
-    F,
-    Case,
-    When,
-    Value,
-    ExpressionWrapper,
-    FloatField,
-    CharField,
-)
+#from django.db.models import (F,Case,When,Value,ExpressionWrapper,FloatField,CharField,)
 from django.db.models.functions import Extract, TruncDate
 
 from attendance.constants import AttendanceStatusChoices
